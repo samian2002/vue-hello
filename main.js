d08a83bcf289d854c1adf64c33d103721e7f7149
samian2002 <samiantariq14@gmail.com>
2024-05-13 17:47:12 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// Inizializziamo Vue.js
+const app = Vue.createApp({
+    // Definiamo i dati
+    data() {
+      return {
+        message: "Hello Vue.Js World"
+      };
+    }
+  });
+  
+
+  app.mount('#app');
+  
